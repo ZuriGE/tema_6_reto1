0928171502dc1f1351c8a49b8641ef543ce228de
ZuriGE <zurigales@gmail.com>
2023-10-16 15:49:49 +0200

    eliminada funcion suma

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-let suma = (x, y=0)=> x+y;
 let resta = (x, y=0)=> x-y;
 let producto = (x, y=0)=> x*y;
 let division = (x, y=0)=> x/y;
 let cuadrado = (x) => x*x
 
-module.exports = {suma, resta, producto, division, cuadrado};
+module.exports = {resta, producto, division, cuadrado};
