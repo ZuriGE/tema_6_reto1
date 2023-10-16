eec88a7f983ee8caa7ff65de24d8195d2be3a771
ZuriGE <zurigales@gmail.com>
2023-10-16 13:00:41 +0200

    subir a remoto

---
+++ index.js
@@ -0,0 +1,7 @@
+let suma = (x, y=0)=> x+y;
+let resta = (x, y=0)=> x-y;
+let producto = (x, y=0)=> x*y;
+let division = (x, y=0)=> x/y;
+let cuadrado = (x) => x*x
+
+module.exports = {suma, resta, producto, division, cuadrado};
